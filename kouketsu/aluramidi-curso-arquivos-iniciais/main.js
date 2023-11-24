981e3272a4a9d1e59a6ab10c6206d0ff01198f9d
jaguarisco <103213790+jaguarisco@users.noreply.github.com>
2023-11-24 08:48:56 -0300

    atualizando minha pagina

---
+++ kouketsu/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,19 @@
+function tocaSomPom() {
+document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+let contadot;
+
+//enquanto
+while(contador < listaDeTeclas.length){
+
+    listaDeTecla[contador].onclick = tocaSomPom
+
+    contador - contador +1;
+
+    console.log(contador);
+}
